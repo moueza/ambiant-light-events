bae94a491ea8d36ec9777295093e4e46ce755a60
moueza <mouezapeter@gmail.com>
2019-06-21 09:49:40 +0200

    v13 ; script in body

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
 
 document.getElementById("toPrint").innerHTML = "toPrint";
 
-document.getElementById("versionn").innerHTML = "version=12";
+document.getElementById("versionn").innerHTML = "version=13";
 
 
 if ('ondevicelight' in window) {
