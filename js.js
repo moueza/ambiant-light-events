f12415148e4679fc64b9bd78ae2ab7e9a838038b
moueza <mouezapeter@gmail.com>
2019-06-21 09:18:00 +0200

    Update js.js

--- js.js
+++ js.js
@@ -4,6 +4,9 @@
 
 
 if ('ondevicelight' in window) {
+        
+   document.getElementById("statuss").innerHTML="lightSensor OK";
+
   window.addEventListener('devicelight', function(event) {
     var body = document.querySelector('body');
     
@@ -11,13 +14,18 @@ if ('ondevicelight' in window) {
     
     
     if (event.value < 50) {
+          document.getElementById("valuee").innerHTML="lightSensor <50";
+
       body.classList.add('darklight');
       body.classList.remove('brightlight');
     } else {
+                document.getElementById("valuee").innerHTML="lightSensor >50";
       body.classList.add('brightlight');
       body.classList.remove('darklight');
     }
   });
 } else {
+      document.getElementById("statuss").innerHTML="lightSensor KO";
+
   console.log('devicelight event not supported');
 }
