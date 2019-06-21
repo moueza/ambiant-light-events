f6525b7362661d36183feab6247918cc710e5b96
moueza <mouezapeter@gmail.com>
2019-06-21 09:47:05 +0200

    v12 ; script in head

--- js.js
+++ js.js
@@ -1,33 +1,33 @@
 
-    
-   document.getElementById("toPrint").innerHTML="toPrint";
-   
-   document.getElementById("versionn").innerHTML="version=11";
+
+document.getElementById("toPrint").innerHTML = "toPrint";
+
+document.getElementById("versionn").innerHTML = "version=12";
 
 
 if ('ondevicelight' in window) {
-        
-   document.getElementById("statuss").innerHTML="lightSensor OK";
 
-  window.addEventListener('devicelight', function(event) {
-    var body = document.querySelector('body');
-    
-    
-    
-    
-    if (event.value < 50) {
-          document.getElementById("valuee").innerHTML="lightSensor level min50";
+    document.getElementById("statuss").innerHTML = "lightSensor OK";
 
-      body.classList.add('darklight');
-      body.classList.remove('brightlight');
-    } else {
-                document.getElementById("valuee").innerHTML="lightSensor level up50";
-      body.classList.add('brightlight');
-      body.classList.remove('darklight');
-    }
-  });
+    window.addEventListener('devicelight', function (event) {
+        var body = document.querySelector('body');
+
+
+
+        document.getElementById("beforeLevel").innerHTML = "lightSensor before level";
+        if (event.value < 50) {
+            document.getElementById("valuee").innerHTML = "lightSensor level min50";
+
+            body.classList.add('darklight');
+            body.classList.remove('brightlight');
+        } else {
+            document.getElementById("valuee").innerHTML = "lightSensor level up50";
+            body.classList.add('brightlight');
+            body.classList.remove('darklight');
+        }
+    });
 } else {
-      document.getElementById("statuss").innerHTML="lightSensor KO";
+    document.getElementById("statuss").innerHTML = "lightSensor KO";
 
-  console.log('devicelight event not supported');
+    console.log('devicelight event not supported');
 }
